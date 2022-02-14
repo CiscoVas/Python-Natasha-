c9a0e8aff0f5c9e0da3a33e1f3f9987b2be111c9
Vassiliy.Borulko <vassiliy.borulko@scity.pro>
2022-02-14 19:37:04 +0600

    Сделаны задания 7_1 7_2 7_2a 7_2b 7_3 7_3a 7_3b

--- exercises/07_files/task_7_2b.py
+++ exercises/07_files/task_7_2b.py
@@ -21,19 +21,49 @@ ignore = ["duplex", "alias", "configuration"]
 from sys import argv
 import os
 
-arg1 = argv[1]
-#arg1 = "config_sw1.txt"
+in_path = str(argv[1])
+out_path = str(argv[2])
+
+add_path = ""
+#add_path = "07_files/"
+
+#in_path = "config_sw1.txt"
+#out_path = "config_sw1_out.txt"
+
+with open(add_path + in_path) as src, open(add_path + out_path, 'w') as dest:
+    for row in src:
+        if row.replace(" ", "") == "" or row.replace(" ", "")[0] == "!":
+            pass
+        else:
+            if ((ignore[0] not in row) 
+                    and ignore[1] not in row
+                    and ignore[2] not in row):
+                dest.write(row)
+
+
+
+'''
+#in_path = argv[1]
+#out_path = str(argv[2])
+in_path = "config_sw1.txt"
+out_path = "config_sw1_out.txt"
 #path = os.getcwd() + "/07_files/"  --- для работы, снизу, чтобы скрипт проверки пройти
 path = os.getcwd() + "/"
 
-f = open ((path + arg1), "r")
-f = f.read().rstrip().split('\n')
+f_input = open((path + in_path), "r")
+f_input = f_input.read().rstrip().split('\n')
 
-for row in f:
+print((path + out_path))
+f_output = open((path + out_path), "w")
+
+for row in f_input:
     if row.replace(" ", "") == "" or row.replace(" ", "")[0] == "!":
         pass
     else:
         if ((ignore[0] not in row) 
                 and ignore[1] not in row
                 and ignore[2] not in row):
-            print(row)
+            f_output.write("\n" + row)
+            
+f_output.close()
+'''

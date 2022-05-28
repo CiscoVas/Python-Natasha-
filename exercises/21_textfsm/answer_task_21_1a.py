179975f0400e2aa81ebecc4a090b43612e979586
Vassiliy.Borulko <vassiliy.borulko@scity.pro>
2022-05-28 13:35:47 +0600

    Сделаны задания 21_1 21_2 21_3 21_4 21_5

--- exercises/21_textfsm/answer_task_21_1a.py
+++ exercises/21_textfsm/answer_task_21_1a.py
@@ -16,25 +16,28 @@
 Проверить работу функции на выводе команды output/sh_ip_int_br.txt
 и шаблоне templates/sh_ip_int_br.template.
 """
-
+from pprint import pprint
 import textfsm
 
+
 def parse_output_to_dict(template, command_output):
-  with open(template) as f_temp:
-    fsm = textfsm.TextFSM(f_temp)
-    l_parse = fsm.ParseText(command_output)
-    head = fsm.header
-  # result = []
-  
-  # for line in l_parse:
-  #   d = {key: val for key, val in zip(head, line)}
-  #   result.append(d)
-  # print(result)
-  
-  return [dict(zip(head, line)) for line in l_parse]
+    with open(template) as tmpl:
+        parser = textfsm.TextFSM(tmpl)
+        header = parser.header
+        result = parser.ParseText(command_output)
+    return [dict(zip(header, line)) for line in result]
+
 
 if __name__ == "__main__":
-  with open("output/sh_ip_int_br.txt") as f:
-    output = f.read()
-  
-  result = parse_output_to_dict("templates/sh_ip_int_br.template", output)
+    with open("output/sh_ip_int_br.txt") as show:
+        output = show.read()
+    result = parse_output_to_dict("templates/sh_ip_int_br.template", output)
+    pprint(result, width=100)
+
+
+# вариант с ParseTextToDicts
+def parse_output_to_dict(template, command_output):
+    with open(template) as template:
+        fsm = textfsm.TextFSM(template)
+        result = fsm.ParseTextToDicts(command_output)
+    return result
